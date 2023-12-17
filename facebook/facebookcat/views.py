676cfcb7919d958a5e3226e797b8e8a7ef577cde
ExecutWBXrn <executwbxrn@gmail.com>
2023-12-17 13:20:17 +0000

    add context data files into temlplates

--- facebook/facebookcat/views.py
+++ facebook/facebookcat/views.py
@@ -2,10 +2,16 @@ from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseNotFound
 
 def index(request):
-    return render(request, "facebookcat/index.html")
+    context = {
+        "title" : "facebook.com",
+    }
+    return render(request, "facebookcat/index.html", context=context)
 
 def about(request):
-    return render(request, "facebookcat/about.html")
+    context = {
+        "title": "facebook.com",
+    }
+    return render(request, "facebookcat/about.html", context=context)
 
 def nfound(request, exception):
     return HttpResponseNotFound("Not facebook")
