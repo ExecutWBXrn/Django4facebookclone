3dfa111a2b03277782e1068cff4ca1ab7d033338
ExecutWBXrn <executwbxrn@gmail.com>
2023-12-18 15:50:04 +0000

    add static files

--- facebook/facebook/settings.py
+++ facebook/facebook/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-hnn$3abq1luww3s%wofw%*gxz^187wp7ou44^_&8k$xu$)exbt'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1']
 
@@ -119,6 +119,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = 'static'
+STATICFILES_DIRS = []
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
